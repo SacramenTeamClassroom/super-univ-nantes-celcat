a31dac248de57292d4f449d479893707437e01b9
Sacramentix <benji.u1225u@gmail.com>
2022-05-30 16:22:16 +0200

    guard all admin route

--- spark-back/src/main/java/back/controller/AdminController.java
+++ spark-back/src/main/java/back/controller/AdminController.java
@@ -2,48 +2,65 @@ package back.controller;
 
 import static spark.Spark.post;
 
+
 import java.util.ArrayList;
 import java.util.Optional;
 
 import back.App;
+import back.model.Password;
 import back.model.Reservation;
 import back.response.Error;
 import back.response.Success;
+import spark.Request;
+import spark.Response;
 
 public class AdminController {
-
-    static String passwordHash = "";
-    static String salt = "";
-
-    public static boolean verifyPassword(String password) {
-        
-        return true;
-    }
     
     public static void init() {
+
         post("/admin/reservation/pending/all", (req, res) -> {
+            if (!guard(req, res)) return guardFail(res);
             return App.gson.toJson(ReservationController.pendingResa);
         });
+
         post("/admin/reservation/accepted/all", (req, res) -> {
+            if (!guard(req, res)) return guardFail(res);
             return App.gson.toJson(ReservationController.acceptedResa);
         });
+
         post("/admin/reservation/refused/all", (req, res) -> {
+            if (!guard(req, res)) return guardFail(res);
             return App.gson.toJson(ReservationController.refusedResa);
         });
+
         post("/admin/reservation/refuse/:uuid", (req, res) -> {
+            if (!guard(req, res)) return guardFail(res);
             var uuid = req.params(":uuid");
             var removed = removeByUuid(ReservationController.pendingResa, uuid);
             if (!removed.isPresent()) return Error.send(res, "No item with "+uuid+" to remove !");
             ReservationController.refusedResa.add(removed.get());
             return Success.send(res, "reservation successfully refused");
         });
+
         post("/admin/reservation/accept/:uuid", (req, res) -> {
+            if (!guard(req, res)) return guardFail(res);
             var uuid = req.params(":uuid");
             var removed = removeByUuid(ReservationController.pendingResa, uuid);
             if (!removed.isPresent()) return Error.send(res, "No item with "+uuid+" to remove !");
             ReservationController.acceptedResa.add(removed.get());
             return Success.send(res, "reservation successfully accepted");
         });
+
+    }
+
+    public static boolean guard(Request req, Response res) {
+        var password = req.headers("Authorization");
+        return Password.verify(password);
+    }
+
+    
+    public static String guardFail(Response res) {
+        return Error.send(res, "Invalid password!");
     }
 
     public static Optional<Reservation> removeByUuid(ArrayList<Reservation> list, String uuid) {
