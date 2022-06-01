c3c07a32d35c1f40795494ee1402b319c9617232
Sacramentix <benji.u1225u@gmail.com>
2022-06-01 09:43:33 +0200

    authorize cors request

--- spark-back/src/main/java/back/App.java
+++ spark-back/src/main/java/back/App.java
@@ -1,6 +1,8 @@
 package back;
 
 import static spark.Spark.port;
+import static spark.Spark.before;
+import static spark.Spark.options;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -21,6 +23,7 @@ public class App {
         try {
             builder = new GsonBuilder();
             gson = builder.create();
+            Password.generateHashAndSalt("admin");
             CelcatJson.GetAll();
             Password.getFromFile();
         } catch (Exception e) {
@@ -34,10 +37,33 @@ public class App {
     public static void start() {
         var port = 4597;
         port(port);
+        addCors();
         AdminController.init();
         CelcatController.init();
         ReservationController.init();
         System.out.println("Start on port " + port);
+    }
+
+    public static void addCors() {
+        options("/*", (req, res) -> {
+            String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+    
+            String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+    
+            return "OK";
+        });
+    
+        before((req, res) -> {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "*");
+            res.type("application/json");
+        });
     }
 
 }
